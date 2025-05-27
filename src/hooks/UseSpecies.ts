07bef4f0b3f6c3e70fc88d0d071520e91addf553
Andres Cagua <andres.fcr089@gmail.com>
2025-05-26 21:27:47 -0500

    fix: prevent unnecessary data loading on initial render in useSpecies hook

--- src/hooks/UseSpecies.ts
+++ src/hooks/UseSpecies.ts
@@ -1,16 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { fetchData, type NormalizedBreed, type Sections } from "@/lib/api";
 import { getLocalStorageItem, setLocalStorageItem } from "@/lib/utils";
 
 export const useSpecies = (section: Sections) => {
+  const firstLoad = useRef(true);
   const [isLoading, setIsLoading] = useState(false);
   const [data, setData] = useState<NormalizedBreed[]>([]);
 
   useEffect(() => {
     const localData: NormalizedBreed[] | null = getLocalStorageItem(section);
 
-    if (localData?.length) {
+    if (localData?.length && !firstLoad.current) {
       setData(localData);
       return;
     }
@@ -27,6 +28,7 @@ export const useSpecies = (section: Sections) => {
     };
 
     fetchSectionData(section);
+    firstLoad.current = false;
   }, [section, data.length]);
 
   return {
